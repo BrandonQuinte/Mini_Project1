07b672b61ce1f4443530b9ffc89351a16e0e2a27
BrandonQuinte <brandonquintero028@gmail.com>
2025-11-06 06:53:58 -0500

    Cambiando de base de deploy

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Mini_Project/', // 👈 importante: debe tener slash inicial y final
+})
+
+ 
