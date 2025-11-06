376ba056116b6d5fd5518d86351f878a3c94ea05
BrandonQuinte <brandonquintero028@gmail.com>
2025-11-06 07:00:38 -0500

    Desplegando proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mini_Project1/', // 👈 importante: debe tener slash inicial y final
+  base: '/Proyecto/', // 👈 importante: debe tener slash inicial y final
 })
 
- 
