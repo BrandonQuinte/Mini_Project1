e91fea1653629508408310e617b6d49ee1b86204
BrandonQuinte <brandonquintero028@gmail.com>
2025-11-06 07:10:54 -0500

    Desplegando proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Mini_Project1/', // 👈 importante: debe tener slash inicial y final
+  base: '/Mini_Project1/', // 👈 usa el nombre EXACTO de tu repo en GitHub
 })
-
